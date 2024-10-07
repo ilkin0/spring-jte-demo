46e872c0e3f4c51d575ef0ae0b6f4d5e092e561d
Ilkin Mehdiyev <ilkin.mehdiyev@ballys.com>
2024-10-07 17:47:22 +0400

    Init commit

---
+++ src/main/java/com/ilkinmehdiyev/springjteblogapp/SpringJteBlogappApplication.java
@@ -0,0 +1,13 @@
+package com.ilkinmehdiyev.springjteblogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJteBlogappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJteBlogappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ilkinmehdiyev/springjteblogapp/SpringJteBlogappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilkinmehdiyev.springjteblogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJteBlogappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
